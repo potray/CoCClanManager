a4cf14be1701845438f35449614c156f8137a417
Daniel <potrayarrick@gmail.com>
2016-01-26 11:40:51 +0100

    Trying to serve static files in production...

--- ResistenciaCoC/settings.py
+++ ResistenciaCoC/settings.py
@@ -113,3 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
